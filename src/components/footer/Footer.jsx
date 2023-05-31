09b658e22895f9f48b7781685807251d425c2a70
ErickMose <erickmongaremose@gmail.com>
2023-05-31 16:41:07 +0300

    feat: add footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <h1>This is the footer</h1>
+      <p>Copyright 2023</p>
+    </footer>
+  );
+};
+
+export default Footer;
